c1c3061a8c232c630058d561938cc8d261801993
Navya Sinha <sinhanavya1875@gmail.com>
2025-09-06 15:03:20 +0530

    updating user info

--- backend/middleware.js
+++ backend/middleware.js
@@ -2,16 +2,18 @@ const jwt = require("jsonwebtoken");
 const { JWT_SECRET } = require("./config");
 
 const authMiddleware = (req, res, next) => {
-  const authheader = req.header.authorization;
+  const authheader = req.headers.authorization;
 
   if (!authheader || !authheader.startsWith("Bearer")) {
-    return res.status(403).json({});
+    return res.status(403).json({
+      message: "Bearer not available",
+    });
   }
-  const token = authheader.split("")[1];
+  const token = authheader.split(" ")[1];
 
   const decodeduser = jwt.verify(token, JWT_SECRET);
   if (decodeduser) {
-    req.userId = decoderuser.id;
+    req.userId = decodeduser.userId;
     next();
   } else {
     return res.status(403).json({});
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -4,6 +4,7 @@ const { JWT_SECRET } = require("../config");
 const router = express.Router();
 const zod = require("zod");
 const User = require("../db");
+const { authMiddleware } = require("../middleware");
 
 const signupBody = zod.object({
   email: zod.string().email(),
@@ -90,4 +91,25 @@ router.post("/signin", async (req, res) => {
   });
 });
 
+const updatedbody = zod.object({
+  password: zod.string().optional(),
+  firstName: zod.string().optional(),
+  lastName: zod.string().optional(),
+});
+
+router.put("/updateinfo", authMiddleware, async (req, res) => {
+  const { success } = updatedbody.safeParse(req.body);
+
+  if (!success) {
+    return res.status(411).json({
+      message: "Error while updating the information",
+    });
+  }
+
+  const result = await User.updateOne({ _id: req.userId }, req.body);
+  console.log(result);
+  res.json({
+    message: "Update happened successfully",
+  });
+});
 module.exports = router;
