55badfd71195f4deb5cc5180c5bf92a3103d746e
Navya Sinha <sinhanavya1875@gmail.com>
2025-09-06 20:00:26 +0530

    filtering option

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -112,4 +112,33 @@ router.put("/updateinfo", authMiddleware, async (req, res) => {
     message: "Update happened successfully",
   });
 });
+
+router.get("/bulk", async (req, res) => {
+  const filter = req.query.filter;
+
+  const users = await User.find({
+    $or: [
+      {
+        firstName: {
+          $regex: filter,
+        },
+      },
+      {
+        lastName: {
+          $regex: filter,
+        },
+      },
+    ],
+  });
+
+  res.json({
+    user: users.map((user) => ({
+      email: user.email,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      _id: user._id,
+    })),
+  });
+});
+
 module.exports = router;
