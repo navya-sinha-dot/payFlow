a1318d3ffd242150cba7f3980c27f4a22a812ce1
Navya Sinha <sinhanavya1875@gmail.com>
2025-10-21 14:52:25 +0530

    changed to prod url

--- frontend/src/lib/utils.js
+++ frontend/src/lib/utils.js
@@ -5,5 +5,5 @@ export function cn(...inputs) {
   return twMerge(clsx(inputs));
 }
 
-export const BACKEND_URL_PROD = "https://pay-flow-fawn.vercel.app/api/v1";
+export const BACKEND_URL_PROD = "https://payflow-be.navysinha.xyz/api/v1";
 export const BACKEND_URL_DEV = "http://localhost:3000/api/v1";
